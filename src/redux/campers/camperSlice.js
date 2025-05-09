e10776c8ef25645157b7e36569d5fa211f52df69
gizemcoban <gizem.coban.r@gmail.com>
2025-05-10 00:02:55 +0200

    campersSlice.js file was set up

---
+++ src/redux/campers/camperSlice.js
@@ -0,0 +1,49 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchCamper, fetchCampers } from './campersOps';
+export const initialState = {
+    name: "campers",
+    campers: [],
+    camper : {},
+    error: null,
+}
+
+export const camperSlice = createSlice({
+    name: initialState.name,
+    initialState,
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchCampers.fulfilled, (state,action) => {
+                state.campers = action.payload;
+                state.error = null;
+                state.camper ={}
+            })
+            .addCase(fetchCampers.pending, (state) => {
+                state.campers = [];
+                state.error = null;
+                state.camper ={};
+            })
+            .addCase(fetchCampers.rejected, (state,action) => {
+                state.campers = [];
+                state.error = action.error;
+                state.camper = {};
+            })
+            .addCase(fetchCamper.fullfilled, (state,action) => {
+                state.camper = action.payload;
+                state.error = null;
+                state.campers = [];
+            })
+            .addCase(fetchCamper.pending, (state) => {
+                state.camper = {};
+                state.error = null;
+                state.campers = [];
+            })
+            .addCase(fetchCamper.rejected, (state,action) => {
+                state.camper = {};
+                state.error = action.error;
+                state.campers = [];
+            })
+
+    }//for builder closure
+}); // for extraReducers closure
+
+export default camperSlice.reducer;
